f5460f6936a90c52f9bf7ddfbec469fc9440f9bf
Douglas Barreto <barreto_oliv@hotmail.com>
2022-02-28 21:10:55 -0300

    Removendo arquivos desnecessários

--- src/index.js
+++ src/index.js
@@ -10,10 +10,7 @@ mongoose.connect(process.env.MONGODB_ACCESS, {
     useUnifiedTopology: true,
 });
 app.use(cors());
-//app.use(cors({ origin: 'http://192.168.1.103:3000'}));
-//app.use(cors({ origin: 'http://localhost:3000'}));
 app.use(express.json({limit: '15mb', extended: true}));
 app.use(express.urlencoded({limit: "15mb", extended: true, parameterLimit:15728640}));
-//app.use(express.json());
 app.use(routes);
 app.listen(process.env.PORT || 3333);
